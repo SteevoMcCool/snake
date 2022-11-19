30485b2de6bfb51ac6b6ac2857d5f4847b8abe97
SteevoMcCool <godzillakirby12@gmail.com>
2022-11-18 23:00:33 -0500

    jjj

--- script.js
+++ script.js
@@ -8,12 +8,30 @@ const DOM = {
   text: document.getElementById("text"),
   score: document.getElementById("score"),
   hscore: document.getElementById("hscore"),
+  hscoreU: document.getElementById("hscoreU"),
   newGame: document.getElementById("newGame"),
+  redP1: document.getElementById("redP1"),
+  greenP1: document.getElementById("greenP1"),
+  blueP1: document.getElementById("blueP1"),
+
+  easyBut: document.getElementById("easy"),
+  medBut: document.getElementById("medium"),
+  hardBut: document.getElementById("hard"),
+  ObFold:document.getElementById("obstacles"),
+  obstacles:[],
+  newGameP2: document.getElementById("newGameP2"),
+  SnFoldP2: document.getElementById("snakeP2"),
+  snakeP2: [],
+  redP2: document.getElementById("redP2"),
+  greenP2: document.getElementById("greenP2"),
+  blueP2: document.getElementById("blueP2"), 
 };
+let difficulty = "medium"
 let started = false;
 let MAIN;
 let score = 0;
 let highscore = 0;
+let hardhighscore = 0;
 let loose = true;
 function loadBoard() {
   for (let x = 0; x < 10; x++) {
@@ -28,8 +46,72 @@ function loadBoard() {
       DOM.tiles[x][y] = newBox;
     }
   }
+  return true
 }
 
+DOM.redP1.addEventListener("click", function(){
+  DOM.SnFold.style.filter = "hue-rotate(0deg)"
+  DOM.redP1.style.border = "2px solid gold"
+  DOM.greenP1.style.border = ""
+  DOM.blueP1.style.border = ""
+})
+DOM.redP1.style.border = "2px solid gold"
+DOM.greenP1.addEventListener("click", function(){
+  DOM.SnFold.style.filter = "hue-rotate(120deg)"
+  DOM.redP1.style.border = ""
+  DOM.greenP1.style.border = "2px solid gold"
+  DOM.blueP1.style.border = ""
+})
+DOM.blueP1.addEventListener("click", function(){
+  DOM.SnFold.style.filter = "hue-rotate(240deg)"
+  DOM.redP1.style.border = ""
+  DOM.greenP1.style.border = ""
+  DOM.blueP1.style.border = "2px solid gold"
+})
+DOM.redP2.addEventListener("click", function(){
+  DOM.SnFoldP2.style.filter = "hue-rotate(0deg)"
+  DOM.redP2.style.border = "2px solid gold"
+  DOM.greenP2.style.border = ""
+  DOM.blueP2.style.border = ""
+})
+DOM.greenP2.addEventListener("click", function(){
+  DOM.SnFoldP2.style.filter = "hue-rotate(120deg)"
+  DOM.redP2.style.border = ""
+  DOM.greenP2.style.border = "2px solid gold"
+  DOM.blueP2.style.border = ""
+})
+DOM.blueP2.style.border = "2px solid gold"
+DOM.blueP2.addEventListener("click", function(){
+  DOM.SnFoldP2.style.filter = "hue-rotate(240deg)"
+  DOM.redP2.style.border = ""
+  DOM.greenP2.style.border = ""
+  DOM.blueP2.style.border = "2px solid gold"
+})
+DOM.easyBut.addEventListener("click", function(){
+  if (!started) {
+    difficulty = "easy"
+    DOM.easyBut.style.border = "2px solid gold"
+    DOM.medBut.style.border = ""
+    DOM.hardBut.style.border = ""
+  }
+})
+DOM.medBut.addEventListener("click", function(){
+  if (!started) {
+    difficulty = "medium"
+    DOM.easyBut.style.border = ""
+    DOM.medBut.style.border = "2px solid gold"
+    DOM.hardBut.style.border = ""
+  }
+})
+DOM.medBut.style.border = "2px solid gold"
+DOM.hardBut.addEventListener("click", function(){
+  if (!started) {
+    difficulty = "hard"
+    DOM.easyBut.style.border = ""
+    DOM.medBut.style.border = ""
+    DOM.hardBut.style.border = "2px solid gold"
+  }
+})
 const snakeBackgroundImage = {
   head: "url('snakeHead.png')",
   b1: "url('SnakeUpDown.png')",
@@ -40,12 +122,21 @@ function genApple(x, y) {
   snakePeiceMoveTo(DOM.apple, x, y);
   appleCoords[0] = x;
   appleCoords[1] = y;
+  return true
 }
 function newGame() {
   DOM.snake.forEach((thing) => {
     thing.remove();
   });
   DOM.snake.length = 0;
+  DOM.obstacles.forEach((thing) => {
+    thing.remove();
+  });
+  DOM.obstacles.length = 0;
+  DOM.snakeP2.forEach((thing) => {
+    thing.remove();
+  });
+  DOM.snakeP2.length = 0;
   genApple(7, 4);
   DOM.snake[0] = document.createElement("div");
   DOM.snake[0].style.position = "absolute";
@@ -57,6 +148,33 @@ function newGame() {
   newSnakePiece();
   snakePeiceMoveTo(DOM.snake[1], 1, 4);
   snakePeiceMoveTo(DOM.snake[0], 2, 4);
+  return true
+}
+function newGameP2() {
+  DOM.snake.forEach((thing) => {
+    thing.remove();
+  });
+  DOM.snake.length = 0;
+  DOM.obstacles.forEach((thing) => {
+    thing.remove();
+  });
+  DOM.obstacles.length = 0;
+  DOM.snakeP2.forEach((thing) => {
+    thing.remove();
+  });
+  DOM.snakeP2.length = 0;
+  genApple(7, 4);
+  DOM.snake[0] = document.createElement("div");
+  DOM.snake[0].style.position = "absolute";
+  DOM.snake[0].style.backgroundImage = snakeBackgroundImage.head;
+  DOM.SnFold.appendChild(DOM.snake[0]);
+  DOM.snake[0].style.transform = "rotate(-90deg)";
+  DOM.snake[0].style.transition = "left 0.2s, top 0.2s";
+  DOM.snake[0].style.zIndex = "2";
+  newSnakePiece();
+  snakePeiceMoveTo(DOM.snake[1], 1, 4);
+  snakePeiceMoveTo(DOM.snake[0], 2, 4);
+  return true
 }
 function snakePeiceMoveTo(p, x, y) {
   if (!DOM.tiles[x]) {
@@ -71,13 +189,16 @@ function snakePeiceMoveTo(p, x, y) {
   p.style.width = `${data.right - data.left}px`;
   p.style.setProperty("--x", x);
   p.style.setProperty("--y", y);
+  return true
+
 }
 function snakePeiceShift(p, x, y) {
   snakePeiceMoveTo(
     p,
     Number(p.style.getPropertyValue("--x")) + x,
-    Number(p.style.getPropertyValue("--y")) + y
-  );
+    Number(p.style.getPropertyValue("--y")) + y )
+    return true
+    
 }
 let direction;
 function interval() {
@@ -99,13 +220,17 @@ function interval() {
     check();
     DOM.snake[0].style.transform = "rotate(0deg)";
   }
+  return true
 }
 function direct(d) {
   if (!started && !loose) {
     MAIN = setInterval(interval, 200);
   }
-  started = true;
-  direction = d;
+  if (!loose){
+    started = true;
+    direction = d;
+  }
+  return true
 }
 document.addEventListener("keydown", (event) => {
   let key = event.key;
@@ -118,6 +243,7 @@ document.addEventListener("keydown", (event) => {
   } else if (key.toLowerCase() == "arrowdown" && direction != "u") {
     direct("d");
   }
+  return true
 });
 function moveSnake() {
   if (DOM.snake.length > 1) {
@@ -132,6 +258,7 @@ function moveSnake() {
       SP.style.transform = PIF.style.transform;
     }
   }
+  return true
 }
 
 function newSnakePiece() {
@@ -150,6 +277,7 @@ function newSnakePiece() {
     PIF.style.getPropertyValue("--x"),
     PIF.style.getPropertyValue("--y")
   );
+  return true
 }
 function findViableSquare() {
   do {
@@ -160,15 +288,38 @@ function findViableSquare() {
         part.style.getPropertyValue("--x") == x &&
         part.style.getPropertyValue("--y") == y
     );
-    if (ar.length == 0) {
+    let ar2 = DOM.obstacles.filter(
+      (part) =>
+        part.style.getPropertyValue("--x") == x &&
+        part.style.getPropertyValue("--y") == y
+    );
+    if (ar.length == 0 && ar2.length == 0) {
+      return [x, y];
+    }
+  } while (true);
+  return true
+}
+function findOPSquare(){
+  do {
+    let x = Math.floor(Math.random() * 10);
+    let y = Math.floor(Math.random() * 10);
+    if (Math.abs(x - DOM.snake[0].style.getPropertyValue("--x"))>2 || Math.abs(y - DOM.snake[0].style.getPropertyValue("--y"))>2 ) {
       console.log(x, y);
       return [x, y];
     }
   } while (true);
 }
+function genObstacle(){
+  let p = DOM.obstacles.length 
+  DOM.obstacles[p] = document.createElement("div")
+  DOM.obstacles[p].style.backgroundColor = "purple"
+  DOM.ObFold.appendChild(DOM.obstacles[p])
+  let pos = findOPSquare()
+  snakePeiceMoveTo(DOM.obstacles[p],pos[0],pos[1])
+}
 function check(lose) {
   if (!started) {
-    return {};
+    return true;
   }
   let head = DOM.snake[0];
   let hx = Number(head.style.getPropertyValue("--x"));
@@ -178,9 +329,18 @@ function check(lose) {
     newSnakePiece();
     let pos = findViableSquare();
     genApple(pos[0], pos[1]);
+    if (difficulty=="medium"){
+      highscore = Math.max(score, highscore);
+    }else if (difficulty =="hard") {
+      hardhighscore = Math.max(score,hardhighscore)
+      genObstacle()
+    }
     DOM.score.innerHTML = `Score: ${score}`;
-    highscore = Math.max(score, highscore);
     DOM.hscore.innerHTML = `High Score: ${highscore}`;
+    DOM.hscoreU.innerHTML = `Hard High Score: ${hardhighscore}`;
+  }
+  if (difficulty=="easy"){
+    return true
   }
   if (
     DOM.snake.filter(
@@ -190,15 +350,22 @@ function check(lose) {
     ).length > 1 ||
     lose
   ) {
-    try {
-      clearInterval(MAIN || setInterval(function () {}, 1000));
       started = false;
       loose = true;
-      console.log(`Started: ${started}`);
-    } catch {
-      console.log("HELP");
-    }
+      direction = "";
+      clearInterval(MAIN || setInterval(function () {}, 1000));
   }
+  if (difficulty=="hard" && DOM.obstacles.filter(
+    (val) =>
+      val.style.getPropertyValue("--x") == hx &&
+      val.style.getPropertyValue("--y") == hy
+  ).length > 0) {
+    started = false;
+    loose = true;
+    direction = "";
+    clearInterval(MAIN || setInterval(function () {}, 1000));  
+  }
+  return true
 }
 
 function setMommySize() {
@@ -210,15 +377,43 @@ function setMommySize() {
     snakePeiceMoveTo(DOM.snake[0], 2, 4);
     snakePeiceMoveTo(DOM.snake[1], 1, 4);
   }
+  DOM.obstacles.forEach((obstacle) => snakePeiceMoveTo(obstacle,obstacle.style.getPropertyValue("--x"),obstacle.style.getPropertyValue("--y")))
   snakePeiceMoveTo(DOM.apple, appleCoords[0], appleCoords[1]);
+  return true
 }
 DOM.newGame.addEventListener("click", function () {
+  console.log("preparing new game...")
   if (!started) {
+    console.log("new gamee...")
     score = 0;
     DOM.score.innerHTML = `Score: ${score}`;
     loose = false;
     newGame();
+  }else {
+    console.log("Failed because started = true")
+    started = false
+    loose = true;
+    direction = "";
+    clearInterval(MAIN || setInterval(function () {}, 1000));
   }
+  return true
+});
+DOM.newGameP2.addEventListener("click", function () {
+  console.log("preparing new game...")
+  if (!started) {
+    console.log("new gamee...")
+    score = 0;
+    DOM.score.innerHTML = `Score: ${score}`;
+    loose = false;
+    newGameP2();
+  }else {
+    console.log("Failed because started = true")
+    started = false
+    loose = true;
+    direction = "";
+    clearInterval(MAIN || setInterval(function () {}, 1000));
+  }
+  return true
 });
 setMommySize();
 window.onresize = setMommySize;
